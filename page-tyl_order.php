fce02c0db91a402cd03a9208afc398b03b326de5
Mark Bloomfield <mark@yellow-llama.com>
2013-01-23 00:12:43 +0200

    form styling

--- page-tyl_order.php
+++ page-tyl_order.php
@@ -1,119 +1,121 @@
 <?php
-// setup for ajax password reset
 
-	global $wpdb, $user_ID;
+/* Template Name: TYL Order Page */
+
 	
-	function tg_validate_url() {
-		global $post;
-		$page_url = esc_url(get_permalink(MAIN_PAGE_ID));
-		$urlget = strpos($page_url, "?");
-		if ($urlget === false) {
-			$concate = "?";
-		} else {
-			$concate = "&";
+	// setup for ajax password reset
+	
+		global $wpdb, $user_ID;
+		
+		function tg_validate_url() {
+			global $post;
+			$page_url = esc_url(get_permalink(MAIN_PAGE_ID));
+			$urlget = strpos($page_url, "?");
+			if ($urlget === false) {
+				$concate = "?";
+			} else {
+				$concate = "&";
+			}
+			return $page_url.$concate;
 		}
-		return $page_url.$concate;
-	}
+		
+		if(isset($_GET['key']) && $_GET['action'] == "reset_pwd") {
+			$reset_key = $_GET['key'];
+			$user_login = $_GET['login'];
+			$user_data = $wpdb->get_row($wpdb->prepare("SELECT ID, user_login, user_email FROM $wpdb->users WHERE user_activation_key = %s AND user_login = %s", $reset_key, $user_login));
+			
+			$user_login = $user_data->user_login;
+			$user_email = $user_data->user_email;
+			
+			if(!empty($reset_key) && !empty($user_data)) {
+				$new_password = wp_generate_password(7, false);
+					//echo $new_password; exit();
+					wp_set_password( $new_password, $user_data->ID );
+					//mailing reset details to the user
+				$message = __('Your new password for the account at:') . "\r\n\r\n";
+				$message .= get_option('siteurl') . "\r\n\r\n";
+				$message .= sprintf(__('Username: %s'), $user_login) . "\r\n\r\n";
+				$message .= sprintf(__('Password: %s'), $new_password) . "\r\n\r\n";
+				$message .= __('You can now login with your new password at: ') . get_option('siteurl')."/login" . "\r\n\r\n";
+				
+				if ( $message && !wp_mail($user_email, 'Password Reset Request', $message) ) {
+					echo "<div class='alert'>Email failed to send for some unknown reason. Try again?</div>";
+					exit();
+				}
+				else {
+					$redirect_to = get_bloginfo('url')."/login?action=reset_success";
+					wp_safe_redirect($redirect_to);
+					exit();
+				}
+			} 
+			else exit('Not a Valid Key.');
+			
+		}
+		//exit();
 	
-	if(isset($_GET['key']) && $_GET['action'] == "reset_pwd") {
-		$reset_key = $_GET['key'];
-		$user_login = $_GET['login'];
-		$user_data = $wpdb->get_row($wpdb->prepare("SELECT ID, user_login, user_email FROM $wpdb->users WHERE user_activation_key = %s AND user_login = %s", $reset_key, $user_login));
-		
-		$user_login = $user_data->user_login;
-		$user_email = $user_data->user_email;
-		
-		if(!empty($reset_key) && !empty($user_data)) {
-			$new_password = wp_generate_password(7, false);
-				//echo $new_password; exit();
-				wp_set_password( $new_password, $user_data->ID );
-				//mailing reset details to the user
-			$message = __('Your new password for the account at:') . "\r\n\r\n";
+		if($_POST['action'] == "tg_pwd_reset"){
+			if ( !wp_verify_nonce( $_POST['tg_pwd_nonce'], "tg_pwd_nonce")) {
+			  exit("No trick please");
+		   }  
+			if(empty($_POST['user_input'])) {
+				echo "<div class='alert'>Please enter your Username or E-mail address</div>";
+				exit();
+			}
+			//We shall SQL escape the input
+			$user_input = $wpdb->escape(trim($_POST['user_input']));
+			
+			if ( strpos($user_input, '@') ) {
+				$user_data = get_user_by_email($user_input);
+				if(empty($user_data) || $user_data->caps[administrator] == 1) { //delete the condition $user_data->caps[administrator] == 1, if you want to allow password reset for admins also
+					echo "<div class='alert'>Invalid E-mail address!</div>";
+					exit();
+				}
+			}
+			else {
+				$user_data = get_userdatabylogin($user_input);
+				if(empty($user_data) || $user_data->caps[administrator] == 1) { //delete the condition $user_data->caps[administrator] == 1, if you want to allow password reset for admins also
+					echo "<div class='alert'>Invalid Username!</div>";
+					exit();
+				}
+			}
+			
+			$user_login = $user_data->user_login;
+			$user_email = $user_data->user_email;
+			
+			$key = $wpdb->get_var($wpdb->prepare("SELECT user_activation_key FROM $wpdb->users WHERE user_login = %s", $user_login));
+			if(empty($key)) {
+				//generate reset key
+				$key = wp_generate_password(20, false);
+				$wpdb->update($wpdb->users, array('user_activation_key' => $key), array('user_login' => $user_login));	
+			}
+			
+			//mailing reset details to the user
+			$message = __('Someone requested that the password be reset for the following account:') . "\r\n\r\n";
 			$message .= get_option('siteurl') . "\r\n\r\n";
 			$message .= sprintf(__('Username: %s'), $user_login) . "\r\n\r\n";
-			$message .= sprintf(__('Password: %s'), $new_password) . "\r\n\r\n";
-			$message .= __('You can now login with your new password at: ') . get_option('siteurl')."/login" . "\r\n\r\n";
+			$message .= __('If this was a mistake, just ignore this email and nothing will happen.') . "\r\n\r\n";
+			$message .= __('To reset your password, visit the following address:') . "\r\n\r\n";
+			$message .= tg_validate_url() . "action=reset_pwd&key=$key&login=" . rawurlencode($user_login) . "\r\n";
 			
 			if ( $message && !wp_mail($user_email, 'Password Reset Request', $message) ) {
-				echo "<div class='alert'>Email failed to send for some unknown reason. Try again?</div>";
+				echo "<div class='alert'>Email failed to send for some unknown reason.</div>";
 				exit();
 			}
 			else {
-				$redirect_to = get_bloginfo('url')."/login?action=reset_success";
-				wp_safe_redirect($redirect_to);
-				exit();
-			}
-		} 
-		else exit('Not a Valid Key.');
-		
-	}
-	//exit();
-
-	if($_POST['action'] == "tg_pwd_reset"){
-		if ( !wp_verify_nonce( $_POST['tg_pwd_nonce'], "tg_pwd_nonce")) {
-		  exit("No trick please");
-	   }  
-		if(empty($_POST['user_input'])) {
-			echo "<div class='alert'>Please enter your Username or E-mail address</div>";
-			exit();
-		}
-		//We shall SQL escape the input
-		$user_input = $wpdb->escape(trim($_POST['user_input']));
-		
-		if ( strpos($user_input, '@') ) {
-			$user_data = get_user_by_email($user_input);
-			if(empty($user_data) || $user_data->caps[administrator] == 1) { //delete the condition $user_data->caps[administrator] == 1, if you want to allow password reset for admins also
-				echo "<div class='alert'>Invalid E-mail address!</div>";
+				echo "<div class='alert alert-success'>We have just sent you an email with password reset instructions.</div>";
 				exit();
 			}
 		}
-		else {
-			$user_data = get_userdatabylogin($user_input);
-			if(empty($user_data) || $user_data->caps[administrator] == 1) { //delete the condition $user_data->caps[administrator] == 1, if you want to allow password reset for admins also
-				echo "<div class='alert'>Invalid Username!</div>";
-				exit();
-			}
-		}
-		
-		$user_login = $user_data->user_login;
-		$user_email = $user_data->user_email;
-		
-		$key = $wpdb->get_var($wpdb->prepare("SELECT user_activation_key FROM $wpdb->users WHERE user_login = %s", $user_login));
-		if(empty($key)) {
-			//generate reset key
-			$key = wp_generate_password(20, false);
-			$wpdb->update($wpdb->users, array('user_activation_key' => $key), array('user_login' => $user_login));	
-		}
-		
-		//mailing reset details to the user
-		$message = __('Someone requested that the password be reset for the following account:') . "\r\n\r\n";
-		$message .= get_option('siteurl') . "\r\n\r\n";
-		$message .= sprintf(__('Username: %s'), $user_login) . "\r\n\r\n";
-		$message .= __('If this was a mistake, just ignore this email and nothing will happen.') . "\r\n\r\n";
-		$message .= __('To reset your password, visit the following address:') . "\r\n\r\n";
-		$message .= tg_validate_url() . "action=reset_pwd&key=$key&login=" . rawurlencode($user_login) . "\r\n";
-		
-		if ( $message && !wp_mail($user_email, 'Password Reset Request', $message) ) {
-			echo "<div class='alert'>Email failed to send for some unknown reason.</div>";
-			exit();
-		}
-		else {
-			echo "<div class='alert alert-success'>We have just sent you an email with password reset instructions.</div>";
-			exit();
-		}
-	}
 
 
-	/* Template Name: TYL Order Page */
+	// call up required scripts & styling
+	wp_enqueue_style('bootstrapcss', get_bloginfo('template_directory').'/tyl_includes/css/bootstrap.css' );
+	wp_enqueue_style('bootstrapresponsive', get_bloginfo('template_directory').'/tyl_includes/css/bootstrap-responsive.css' );
+	wp_enqueue_style('style', get_bloginfo('template_directory').'/tyl_includes/css/tyl_style.css' );
 	
-
-wp_enqueue_style('bootstrapcss', get_bloginfo('template_directory').'/tyl_includes/css/bootstrap.css' );
-wp_enqueue_style('bootstrapresponsive', get_bloginfo('template_directory').'/tyl_includes/css/bootstrap-responsive.css' );
-wp_enqueue_style('style', get_bloginfo('template_directory').'/tyl_includes/css/style.css' );
-
-wp_enqueue_script('bootstrap', get_bloginfo('template_directory').'/tyl_includes/js/bootstrap.js', array('jquery'), '1.0' );
-wp_enqueue_script('tab', get_bloginfo('template_directory').'/tyl_includes/js/bootstrap-tab.js', array('jquery'), '1.0' );
-wp_enqueue_script('theme', get_bloginfo('template_directory').'/tyl_includes/js/theme.js', array('jquery'), '1.0' );
+	wp_enqueue_script('bootstrap', get_bloginfo('template_directory').'/tyl_includes/js/bootstrap.js', array('jquery'), '1.0', true );
+	wp_enqueue_script('tyl_app', get_bloginfo('template_directory').'/tyl_includes/js/tyl_app.js', array('jquery'), '1.0', true );
+	wp_enqueue_script('theme', get_bloginfo('template_directory').'/tyl_includes/js/theme.js', array('jquery'), '1.0', true );
 
 get_header(); ?>
 
--- tyl_includes/tylmarketingtool/user.php
+++ tyl_includes/tylmarketingtool/user.php
@@ -5,16 +5,34 @@
 	<?php tylMain() ?>
 	
 <?php } else { ?>
-	
-	<ul class="nav nav-tabs" id="usertools">
-		<li class="active"><a href="#login">Login</a></li>
-		<li><a href="#register">Register</a></li>
-		<li><a href="#forgot">Lost Password?</a></li>
+
+	<ul class="tyl_nav nav-tabs" id="usertools">
+		<li class="active"><a href="#login" data-target="login">Login</a></li>
+		<li><a href="#register" data-target="register">Register</a></li>
+		<li><a href="#forgot" data-target="forgot">Lost Password?</a></li>
 	</ul>
 	
 	<div class="tab-content">
 		<div class="tab-pane active" id="login">
-			<?php echo wp_login_form(); ?>
+			<form name="loginform" id="loginform" action="<?php echo bloginfo('url'); ?>/wp-login.php" method="post" class="">
+				<div class="input-prepend">
+					<span class="add-on">Email Address</span>
+					<input type="text" name="log" id="user_login" class="" value="" />
+				</div>			
+				<div class="input-prepend">
+					<span class="add-on">Password</span>
+					<input type="password" name="pwd" id="user_pass" class="" value="" />
+				</div>			
+				
+				<div class="input-prepend">
+					<label class="checkbox"><input type="checkbox" name="rememberme" value="forever"> Remember me</label>
+				</div>			
+
+				<div class="clearfix buttonwrap">
+					<input type="submit" name="wp-submit" id="wp-submit" class="btn btn-primary" value="Log In" />
+					<input type="hidden" name="redirect_to" value="<?php echo get_permalink(MAIN_PAGE_ID); ?>" />
+				</div>				
+			</form>
 		</div><!-- login -->
 		
 		<div class="tab-pane" id="register">
@@ -23,15 +41,21 @@
 		
 		<div class="tab-pane" id="forgot">
 			<div id="result"></div>
+
 			<form id="wp_pass_reset" action="" method="post">
-			 
-			<label for="user_login">Enter your username or email address: </label>
-			<input type="text" name="user_input" id="user_login" value="" /><br />
-			<input type="hidden" name="action" value="tg_pwd_reset" />
-			<input type="hidden" name="tg_pwd_nonce" value="<?php echo wp_create_nonce("tg_pwd_nonce"); ?>" />
-			<input type="submit" id="submitbtn" name="submit" class="btn btn-primary" value="Reset my password" />
-			 
+				<div class="input-prepend">
+					<span class="add-on">Username or email address</span>
+					<input type="text" name="user_input" id="user_login" value="" />
+				</div>			
+
+				<input type="hidden" name="action" value="tg_pwd_reset" />
+				<input type="hidden" name="tg_pwd_nonce" value="<?php echo wp_create_nonce("tg_pwd_nonce"); ?>" />
+
+				<div class="clearfix buttonwrap">
+					<input type="submit" id="submitbtn" name="submit" class="btn btn-primary" value="Reset my password" />
+				</div>
 			</form>
+
 			<script type="text/javascript">
 			jQuery("#wp_pass_reset").submit(function() {
 				jQuery("#result").html("<div class='alert alert-info alert-validating'>Validating...</div>").slideDown();
